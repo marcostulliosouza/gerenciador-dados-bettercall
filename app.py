18deb4f711a1676241133e3e7b4f0a47e61ae230
Marcos Souza <marcostullio.s@gmail.com>
2024-08-08 14:17:12 -0300

    Corrigido e adicionado a pasta assets no bdist

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import bd
 import configparser
 import hashlib
 
-__version__ = '1.3'
+__version__ = '1.4'
 
 
 class Aplicacao:
--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 from cx_Freeze import setup, Executable
+import os
 
 modules = ['configparser', 'PIL']
 
 packages = ['encodings']
 
-include_files = ['dbconfig.ini', 'assets/seta_direita.png', 'assets/seta_esquerda.png']
+include_files = [
+    'dbconfig.ini',
+    ('assets/', 'assets/'),  # Incluir o diretório 'assets' e seu conteúdo
+]
 
 executables = [Executable('app.py', base='Win32GUI')]
 
 setup(
     name='Gerenciador Dados BetterCall',
-    version='1.3',
+    version='1.4',
     options={
         'build_exe': {
             'packages': modules + packages,
             'include_files': include_files,
             'include_msvcr': True,
+        },
+        'bdist_msi': {
+            'add_to_path': False,  # Ajuste conforme necessário
+            'upgrade_code': '1ecdae60-d54b-4ec1-b553-a6367d5c1934',  # Substitua pelo GUID gerado: https://www.guidgenerator.com/
         }
     },
     executables=executables
